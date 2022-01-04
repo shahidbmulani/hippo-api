f1de495898c0d131c2970467983ebc4f3811c56a
Shahid Mulani <shahidm@techspawn.co>
2022-01-04 22:04:38 +0530

    initial commit

---
+++ includes/class-hippo-api-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/shahidbmulani
+ * @since      1.0.0
+ *
+ * @package    Hippo_Api
+ * @subpackage Hippo_Api/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Hippo_Api
+ * @subpackage Hippo_Api/includes
+ * @author     Shahid Mulani <shahid.mulani123@gmail.com>
+ */
+class Hippo_Api_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-hippo-api-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/shahidbmulani
+ * @since      1.0.0
+ *
+ * @package    Hippo_Api
+ * @subpackage Hippo_Api/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Hippo_Api
+ * @subpackage Hippo_Api/includes
+ * @author     Shahid Mulani <shahid.mulani123@gmail.com>
+ */
+class Hippo_Api_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
